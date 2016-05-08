89370e7b8a3b1635f5f242c07e8c16f7041d61fd
PPinto22 <pedropinto24@hotmail.com>
2016-05-08 14:08:21 +0100

    ...

--- Floresta/main.cpp
+++ Floresta/main.cpp
@@ -31,7 +31,8 @@ int draw_mode = 0; //0 = Fill, 1 = Line, 2 = Point
 unsigned int t, tw, th;
 unsigned char *imageData;
 
-GLuint* buffers;
+GLuint buffers[1];
+GLuint normals;
 
 void sphericalToCartesian() {
 
@@ -92,6 +93,16 @@ void renderScene(void) {
 	gluLookAt(camX, camY, camZ,
 		0.0, 0.0, 0.0,
 		0.0f, 1.0f, 0.0f);
+	
+	GLfloat amb[4] = { 0.2, 0.2, 0.2, 1.0 };
+	GLfloat diff[4] = { 1.0, 1.0, 1.0, 1.0 };
+	GLfloat pos[4] = { 0.0, 30.0 ,30.0, 0.0 };
+
+	// light position
+	glLightfv(GL_LIGHT0, GL_POSITION, pos);
+	// light colors
+	glLightfv(GL_LIGHT0, GL_AMBIENT, amb);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, diff);
 
 	// Drawing Mode
 	switch (draw_mode) {
@@ -111,8 +122,12 @@ void renderScene(void) {
 	glVertexPointer(3, GL_FLOAT, 0, 0);
 	glColor3d(1, 1, 1);
 	for (int i = 0; i < th - 1; i++) {
-		int inicio = i * 2 * th;
-		int count = 2 * th;
+		int inicio = i * 2 * (th-2);
+		int count = 2 * (th-2);
+
+		glBindBuffer(GL_ARRAY_BUFFER, normals);
+		glNormalPointer(GL_FLOAT, 0, 0);
+
 		glDrawArrays(GL_TRIANGLE_STRIP, inicio, count);
 	}
 
@@ -161,6 +176,17 @@ void renderScene(void) {
 	glutSwapBuffers();
 }
 
+void cross(float *a, float *b, float *res) {
+	res[0] = a[1] * b[2] - a[2] * b[1]; res[1] = a[2] * b[0] - a[0] * b[2]; res[2] = a[0] * b[1] - a[1] * b[0];
+}
+
+void normalize(float *a) {
+	float l = sqrt(a[0] * a[0] + a[1] * a[1] + a[2] * a[2]);
+	a[0] = a[0] / l;
+	a[1] = a[1] / l;
+	a[2] = a[2] / l;
+}
+
 void loadTerrain() {
 	ilGenImages(1, &t);
 	ilBindImage(t);
@@ -169,24 +195,68 @@ void loadTerrain() {
 	tw = ilGetInteger(IL_IMAGE_WIDTH);
 	th = ilGetInteger(IL_IMAGE_HEIGHT);
 	imageData = ilGetData();
-	buffers = (GLuint*)malloc(1 * sizeof(GLuint));
-	float* vertexB = (float*)malloc(sizeof(float)*(th-1)*tw*6);
+	float* vertexB = (float*)malloc(sizeof(float)*(th-2)*(tw-2)*6);
+	float* normalB = (float*)malloc(sizeof(float)*(th - 2)*(tw - 2) *6);
 	int dx = th / 2;
 	int dz = tw / 2;
-	int p = 0;
-	for (int z = 0; z < th-1; z++) {
-		for (int x = 0; x < tw; x++) {
-			vertexB[p++] = x - dx;
-			vertexB[p++] = h(z,x);
-			vertexB[p++] = z - dz;
+	int pi = 0, ni = 0;
+	for (int z = 1; z < th-1; z++) {
+		for (int x = 1; x < tw - 1; x++) {
+			float v1[3], v2[3], n[3];
 
-			vertexB[p++] = x - dx;
-			vertexB[p++] = h(z+1,x);
-			vertexB[p++] = z + 1 - dz;
+			//ponto (x,y,z)
+			vertexB[pi++] = x - dx;
+			vertexB[pi++] = h(z,x);
+			vertexB[pi++] = z - dz;
+
+			v1[0] = 0;
+			v1[1] = h(z + 1, x) - h(z - 1, x);
+			v1[2] = 2;
+
+			v2[0] = 2;
+			v2[1] = h(z, x + 1) - h(z, x - 1);
+			v2[2] = 0;
+
+			normalize(v1); normalize(v2);
+			cross(v1, v2, n);
+			normalize(n);
+
+			normalB[ni++] = n[0];
+			normalB[ni++] = n[1];
+			normalB[ni++] = n[2];
+
+			//ponto (x,y,z+1)
+			vertexB[pi++] = x - dx;
+			vertexB[pi++] = h(z+1,x);
+			vertexB[pi++] = z + 1 - dz;
+
+			v1[0] = 0;
+			v1[1] = h(z + 1 + 1, x) - h(z + 1 - 1, x);
+			v1[2] = 2;
+
+			v2[0] = 2;
+			v2[1] = h(z + 1, x + 1) - h(z + 1, x - 1);
+			v2[2] = 0;
+
+			normalize(v1); normalize(v2);
+			cross(v1, v2, n);
+			normalize(n);
+
+			normalB[ni++] = n[0];
+			normalB[ni++] = n[1];
+			normalB[ni++] = n[2];
 		}
 	}
+
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glGenBuffers(1, buffers);
 	glBindBuffer(GL_ARRAY_BUFFER, buffers[0]);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(float)*(th-1)*tw*6, vertexB, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float)*(th-2)*(tw-2)*6, vertexB, GL_STATIC_DRAW);
+
+	glEnableClientState(GL_NORMAL_ARRAY);
+	glGenBuffers(1, &normals);
+	glBindBuffer(GL_ARRAY_BUFFER, normals);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float)*(th - 2)*(tw - 2) * 6, normalB, GL_STATIC_DRAW);
 }
 
 void menuHandler(int id_op) {
@@ -267,6 +337,9 @@ int main(int argc, char **argv) {
 	loadTerrain();
 
 	// OpenGL settings 
+	glEnable(GL_TEXTURE_2D);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_CULL_FACE);
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
